9d82a14b4d79f99b0c4e2a404082a3805192b8e0
Stefan Riedel | alphabytes <sr@alphabytes.de>
2016-03-15 17:57:55 +0100

    begin event args class

---
+++ src/Exceptions/EventException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2016. Stefan Riedel <sr_at_srit83.de>
+ * This software is licensed in gplv3 http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: stefanriedel
+ * Date: 15.03.16
+ * Time: 17:37
+ */
+
+namespace Lava83\LavaProto\Exceptions;
+
+/**
+ * Class EventException
+ * @package Lava83\LavaProto\Exceptions
+ */
+class EventException extends \Exception
+{
+
+}
